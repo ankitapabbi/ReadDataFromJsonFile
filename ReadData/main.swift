82bb0b6584296f05bd4d5779d4167804387f27fa
Ankita Pabbi <erankitapabbi@gmail.com>
2019-06-14 18:14:48 -0400

    Initial Commit

---
+++ ReadData/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ReadData
+//
+//  Created by Ankita Pabbi on 2019-06-14.
+//  Copyright © 2019 Ankita Pabbi. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
